afd4332d7a1ca0d335f7e56116fe355d970d9724
Ariqfzn <fauzanariq88@gmail.com>
2022-03-22 08:30:35 +0700

    menambahkan file baru

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya Ariq Fauzan
+              saya tinggal di daerah ciputat
+              olah raga yang saya sukai adalah
+              BasketBall";
+    }
+}
