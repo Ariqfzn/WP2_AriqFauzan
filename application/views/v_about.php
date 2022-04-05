680daff368cb5b49b8d9f9e0ca3ebffa680ba655
Ariqfzn <fauzanariq88@gmail.com>
2022-04-05 10:39:56 +0700

    menambahkan file pertemuan 3

---
+++ application/views/v_about.php
@@ -0,0 +1,24 @@
+<section>     
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>     
+    <ul type="disc">         
+        <li>Nama Depan : Ariq</li>         <li>Nama Belakang : Fauzan</li>     
+    </ul>     
+    <br>     
+    <h4>Alamat</h4>     
+    <ul type="none">         
+        <li> 
+            Jalan Cimandiri 4 RT 001 RW 07 Cipayung Ciputat
+        </li>     
+    </ul>          
+    <h4>Tempat Lahir</h4>     
+    <ul type="none">         
+        <li>Rumah Sakit</li>     
+    </ul> 
+ 
+    <h4>Olah Raga Favorit</h4>     
+    <ul type="square">         
+        <li>Futsal</li>         
+        <li>Gaple (Dominos)</li>     
+    </ul> 
+</section>
---
+++ application/views/v_footer.php
@@ -0,0 +1,5 @@
+    <footer>
+        <a href="http://www.RentalBuku.com"><a>RentalBuku</a>
+    </footer>
+</body>
+</html>
